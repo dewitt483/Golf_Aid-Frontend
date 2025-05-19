bf165437d906fec66bfa78a51f9a2ff4f21e996e
DeWitt Colvin <colgd-25@rhodes.edu>
2025-05-19 11:03:21 -0500

    creates front end base code

---
+++ Golf_Aid/Golf_Aid/Golf_AidApp.swift
@@ -0,0 +1,17 @@
+//
+//  Golf_AidApp.swift
+//  Golf_Aid
+//
+//  Created by DeWitt Colvin on 5/19/25.
+//
+
+import SwiftUI
+
+@main
+struct Golf_AidApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Golf_Aid/Golf_AidTests/Golf_AidTests.swift
@@ -0,0 +1,17 @@
+//
+//  Golf_AidTests.swift
+//  Golf_AidTests
+//
+//  Created by DeWitt Colvin on 5/19/25.
+//
+
+import Testing
+@testable import Golf_Aid
+
+struct Golf_AidTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
